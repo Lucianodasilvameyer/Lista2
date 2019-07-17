8a557d5fa3a6d631de272cad617f153311b1d385
Lucianodasilvameyer <murcilhax@hotmail.com>
2019-07-17 12:23:19 -0300

    projeto inicial

---
+++ Lista2/Assets/Player.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void andar()
+    {
+
+        Vector2 input = new Vector2(Input.GetAxisRaw("Horizontal"), 0);
+
+
+        Vector3 direction = input.normalized;
+
+        Vector3 velocity = direction * speed;
+
+
+        Vector3 moveAmount = velocity * Time.deltaTime;
+
+
+        transform.Translate(moveAmount);
+    }
+}
