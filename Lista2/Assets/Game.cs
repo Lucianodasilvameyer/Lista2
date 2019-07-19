a58d9febed830921762c51187fec263736c4dd6a
Lucianodasilvameyer <murcilhax@hotmail.com>
2019-07-19 10:46:53 -0300

    mais inimigos

--- Lista2/Assets/Game.cs
+++ Lista2/Assets/Game.cs
@@ -13,19 +13,20 @@ public class Game : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        spawnEnemy(new Vector2(Random.Range(3,8),Random.Range(4,7)));  
+        spawnEnemy(new Vector2(Random.Range(3,8),Random.Range(4,2)));
+        spawnEnemy(new Vector2(Random.Range(9, 1), Random.Range(5, 7)));
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+       
     }
 
     void spawnEnemy(Vector2 position)
     {
         GameObject go = Instantiate(inimigoPrefab, position, Quaternion.identity);// o Quaternion.identity é para manter a mesma rotação
-        go.GetComponent<Inimigo>().direction = (new Vector3(Random.Range(3, 8), Random.Range(4, 7)) - go.transform.position).normalized;
+        go.GetComponent<Inimigo>().direction = (new Vector3(Random.Range(3, 8), Random.Range(4, 2)) - go.transform.position).normalized;
         listaInimigos.Add(go.GetComponent<Inimigo>());
 
         
